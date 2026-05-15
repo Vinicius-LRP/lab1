e4596a63fcf6fdb36b38e20b4fb3bed516be361c
Vinicius Lucca Righi Padilha <viniciusluccarighi@gmail.com>
2026-05-15 20:25:32 -0300

    Adicionando exercicio 8 em aula 19

--- Aulas/a19/Exercicio8.c
+++ Aulas/a19/Exercicio8.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
 
 typedef struct{
+    char rua[50];
+    int número;
+    char cidade[50];
+}Endereco;
+
+typedef struct{
     char nome[50];
-    float valor;
-}Produto;
-
-void aumentarPreco(Produto *p){
-    p->valor *= 1.1;
-}
-void detalhesProduto(Produto p){
-    printf("Nome do produto: %s\n", p.nome);
-    printf("Valor do produto: %.2f\n", p.valor);
-
-}
-
-
+    int idade;
+    Endereco endereco;
+}Pessoa;
 
 int main(){
-    Produto escova = {"Escova Eletrica", 15.99};
-   
-    Produto produtos[5] = {escova};
-    aumentarPreco(&produtos[0]);
-    detalhesProduto(produtos[0]);
+    Pessoa pessoa1 = {"Eduardo", 22, {"Conselheiro Pereiron", 620, "Brasilia"}};
 }
