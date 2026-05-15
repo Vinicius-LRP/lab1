d8599a007604fd08809a37d8817d42f32589fb1d
Vinicius Lucca Righi Padilha <viniciusluccarighi@gmail.com>
2026-05-14 21:17:41 -0300

    Exercicio 6 da Aula 19

---
+++ Aulas/a19/Exercicio6.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    char titulo[50];
+    char autor[50];
+    int ano;
+}Livro;
+
+void le_titulo(int indice,Livro l[]){
+    int a = 0;
+    for(; ;){
+        scanf("%c", &l[indice].titulo[a]);
+        if(l[indice].titulo[a] == '\n') break;
+        a++;
+    }
+    l[indice].titulo[a] = '\0';
+}
+void le_autor(int indice, Livro l[]){
+    int a = 0;
+    for(;; ){
+        scanf("%c", &l[indice].autor[a]);
+        if(l[indice].autor[a] == '\n') break;
+        a++;
+    }
+    l[indice].autor[a] = '\0';
+}
+
+void adicionar_livros(int num_livros, Livro l[num_livros]){
+    for(int a = 0 ; a < num_livros ; a++){
+        printf("Digite o título do livro: ");
+        le_titulo(a, l);
+        printf("Digite o nome do autor: ");
+        le_autor(a, l);
+        printf("Digite o ano de publicação: ");
+        scanf("%d", &l[a].ano);
+        char c = getchar();
+    }
+}
+
+Livro livro_mais_antigo(int num_livros,Livro l[num_livros]){
+    int mais_antigo = l[0].ano;
+    int indice;
+    for(int a = 0; a < num_livros; a++){
+        if(l[a].ano < mais_antigo) indice = a;
+    }
+    return l[indice];
+}
+
+void imprime_livro(Livro l){
+    printf("Título: %s\n", l.titulo);
+    printf("Autor: %s\n", l.autor);
+    printf("Ano: %d\n", l.ano);
+}
+
+
+
+int main(){
+    Livro livros[5];
+
+    adicionar_livros(3, livros);
+
+    Livro livro_antigo = livro_mais_antigo(3, livros);
+
+    printf("Livro mais antigo");
+    imprime_livro(livro_antigo);
+
+
+}
