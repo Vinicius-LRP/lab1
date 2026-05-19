daf3eac2b6e8a7239f10f74cdd7d7b3df2f2ad3d
vinirighi <viniciusluccarighi@gmail.com>
2026-05-19 10:08:46 -0300

    Adicionando exercicios aula 19

--- Aulas/a19/Exercicio24.c
+++ Aulas/a19/Exercicio24.c
@@ -25,15 +25,7 @@ typedef struct{
 int acha_postit(int n, postit v[n], ponto p){
     n -= 1;
     for(; n >= 0 ; n--){
-        printf("N: %d\n", n);
-        printf("r inf_esq x: %d\n",v[n].r.inf_esq.x);
-        printf("r inf_esq y: %d\n",v[n].r.inf_esq.y);
-        printf("r sup_dir x: %d\n",v[n].r.sup_dir.x);
-        printf("r sup_dir y: %d\n",v[n].r.sup_dir.y);
-        printf("X: %d\n", p.x);
-        printf("Y: %d\n", p.y);
-
-        if(p.x >= v[n].r.inf_esq.x && p.x <= v[n].r.sup_dir.x && p.y >= v[n].r.inf_esq.y && p.y <= v[n].r.sup_dir.y)
+            if(p.x >= v[n].r.inf_esq.x && p.x <= v[n].r.sup_dir.x && p.y >= v[n].r.inf_esq.y && p.y <= v[n].r.sup_dir.y)
             return n;
     }
     return -1;
@@ -53,7 +45,7 @@ int main(){
     postit postits[50] = {{"postit 1", { { 3 , 3 } , { 1 , 1 } } }, {"postit 2", { { 5 , 5 } , { 2 , 2 } } } };
     ponto p = {2,2};
 
-    
+
     imprimepostit(postits[acha_postit(2, postits, p)]);
 
 }
