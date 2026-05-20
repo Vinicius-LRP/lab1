01da26954e3c46359171b42b53bcaa5a1f6bbefd
vinirighi <viniciusluccarighi@gmail.com>
2026-05-20 20:40:52 -0300

    Adicionando exercicios aula 22

---
+++ Aulas/a22/Exercicio1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool estaNoVetor(int tam, int v[tam], int num){
+    for(int a = 0; a < tam ; a++){
+        if(v[a] == num) return true;
+    }
+    return false;
+}
+
+
+
+
+int main(){
+    int vet[3] = {2, 3, 5};
+
+    printf("%d", estaNoVetor(3, vet, 2));
+
+
+}
