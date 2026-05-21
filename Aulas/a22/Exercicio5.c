10cfddb5e63930990884b3310cd207a7a80096f2
vinirighi <viniciusluccarighi@gmail.com>
2026-05-20 21:39:48 -0300

    Adicionando exercicios aula 22

--- Aulas/a22/Exercicio5.c
+++ Aulas/a22/Exercicio5.c
@@ -6,9 +6,14 @@ typedef struct{
     int superior;
 }Intervalo;
 
+bool estaNoIntervalo(int num, Intervalo i){
+    if(num >= i.inferior && num <= i.superior) return 1;
+    return 0;
+}
+
 bool estaNoVetor(int tam, Intervalo i[tam], int num){
     for(int a = 0; a < tam ; a++){
-        if(num >= i[a].inferior && num <= i[a].superior) return true;
+        if(verificandoIntervalo(num, i[a])) return true;
     }
     return false;
 }
@@ -16,25 +21,24 @@ bool estaNoVetor(int tam, Intervalo i[tam], int num){
 int qntsVezesNoVetor(int tam, Intervalo i[tam], int num){
     int vezes = 0;
     for(int a = 0; a < tam ; a++){
-        if(num >= i[a].inferior && num <= i[a].superior) vezes++;
+        if(verificandoIntervalo(num, i[a])) vezes++;
     }
     return vezes;
 }
 
 int posicaoNoVetor(int tam, Intervalo i[tam], int num){
     for(int a = 0; a < tam; a++){
-        if(num >= i[a].inferior && num <= i[a].superior) return a;
+        if(verificandoIntervalo(num, i[a])) return a;
     }
     return -1;
 }
 
 int ultimaPosicaoNoVetor(int tam, Intervalo i[tam], int num){
     for(int a = tam - 1; a >= 0 ; a--){
-        if(num >= i[a].inferior && num <= i[a].superior) return a;
+        if(verificandoIntervalo(num, i[a])) return a;
     }
     return -1;
 }
-
 
 int main(){
     Intervalo intervalos[3] = {{2, 3},{3, 5},{4, 6}};
@@ -42,5 +46,6 @@ int main(){
     printf("%d\n", estaNoVetor(3, intervalos, 1));
     printf("%d\n", qntsVezesNoVetor(3, intervalos, 4));
     printf("%d\n", posicaoNoVetor(3, intervalos, 5));
-    printf("%d\n", ultimaPosicaoNoVetor(3, intervalos, 2));
+    printf("%d\n", ultimaPosicaoNoVetor(3, intervalos, 4));
+
 }
