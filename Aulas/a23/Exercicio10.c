782cc820b726530b7ed7f40516ca6103dd48a761
vinirighi <viniciusluccarighi@gmail.com>
2026-05-22 20:55:12 -0300

    Adicionando exercicios aula 23

---
+++ Aulas/a23/Exercicio10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int quantidadeDeCaracteres(FILE *arq){
+    int quantidade = 0;
+    int c = fgetc(arq);
+    while(c != EOF){
+        quantidade++;
+        c = fgetc(arq);
+    }
+    return quantidade;
+}
+
+
+
+int main(){
+    FILE *arquivo = fopen("arquivo.txt", "r");
+
+    if(arquivo == NULL){
+        printf("Erro ao abrir arquivo!");
+        return 1;
+    }
+
+    printf("%d\n", quantidadeDeCaracteres(arquivo));    
+    
+    fclose(arquivo);
+    return 0;
+}
