0372359286e9a23cdef35e67377695682940ae15
vinirighi <viniciusluccarighi@gmail.com>
2026-05-24 01:17:53 -0300

    Adicionando exercicios aula 23

---
+++ Aulas/a23/Exercicios22.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int maiorValor(FILE *a){
+    int c;
+    int num = 0;
+    int maior;
+    while(!feof(a)){
+        c = fgetc(a);
+        if(c != EOF && c != '\n' && c != ' '){
+            num = num * 10 + (c - '0');
+        } else{
+            if(num > maior) maior = num;
+            num = 0;
+        }
+    }
+    return maior;
+}
+
+
+int main(){
+    FILE *arq = fopen("arquivo.txt", "r");
+    if(arq == NULL){
+        printf("Erro ao abrir arquivo!");
+        return 1;
+    }
+
+    printf("Maior valor: %d\n", maiorValor(arq));
+
+
+
+    fclose(arq);
+    return 0;
+}
