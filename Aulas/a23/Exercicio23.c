9eb8f5294c7710e661fa1e42e48d45fb39c20df3
vinirighi <viniciusluccarighi@gmail.com>
2026-05-24 01:41:46 -0300

    Adicionando exercicios aula 23

--- Aulas/a23/Exercicio23.c
+++ Aulas/a23/Exercicio23.c
@@ -2,16 +2,16 @@
 
 int main(){
     FILE *arq = fopen("arquivo.txt", "r");
+    
     if(arq == NULL){
         printf("Erro ao abrir arquivo!");
         return 1;
     }
-    char string[5];
+    char string[100];
 
-    while(fgets(string, 5, arq) != NULL){
+    while(fgets(string, 100, arq) != NULL){
         printf("%s", string);
     }
-    printf("\n");
 
     fclose(arq);
     return 0;
