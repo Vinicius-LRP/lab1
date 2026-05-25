8e30247860e4d405b33fa569d3dd9821e1352563
Vinicius Lucca Righi Padilha <viniciusluccarighi@gmail.com>
2026-05-25 20:20:35 -0300

    Adicionando exercicios aula 23

--- Aulas/a23/Exercicio34.c
+++ Aulas/a23/Exercicio34.c
@@ -3,25 +3,31 @@
 int contarInteiros(char nome[]){
     FILE *arq = fopen(nome, "r");
     if(arq == NULL){
-        return -1;
+        return -2;
     }
     int quantidade = 0;
     int numero;
+    int r;
     
-    while(fscanf(arq, "%d", &numero) == 1)
+    while((r = fscanf(arq, "%d", &numero)) != EOF){
+        if(r == 0 && !feof(arq)){
+            fclose(arq);
+            return -1;
+        }
         quantidade++;
-
+    }
     fclose(arq);
     return quantidade;
-
 }
 
 int main(){
     char arquivo[50] = "arquivo.txt";
     int resultado = contarInteiros(arquivo);
 
-    if(resultado == -1){
+    if(resultado == -2){
         printf("Erro ao abrir arquivo!\n");
+    } else if(resultado == -1){
+        printf("Não há somente números!");
     } else {
         printf("Quantidade: %d\n", resultado);
     }
