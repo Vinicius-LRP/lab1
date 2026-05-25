514949cd5c0c84f52712a7f3c97c399fadaf6644
Vinicius Lucca Righi Padilha <viniciusluccarighi@gmail.com>
2026-05-25 18:27:30 -0300

    Adicionando exercicios aula 23

--- Aulas/a23/Exercicio30.c
+++ Aulas/a23/Exercicio30.c
@@ -2,7 +2,7 @@
 
 typedef struct{
     char nome[50];
-    char idade[3];
+    char idade[50];
     char curso[50];
 }Aluno;
 
@@ -10,12 +10,15 @@ void cadastrarAluno(FILE *a){
     Aluno al;
     printf("Digite o nome: ");
     fgets(al.nome, 50, stdin);
+    fputs("Nome: ", a);
     fputs(al.nome, a);
     printf("Digite a idade: ");
-    fgets(al.idade, 3, stdin);
+    fgets(al.idade, 50, stdin);
+    fputs("Idade: ", a);
     fputs(al.idade, a);
     printf("Digite o curso: ");
     fgets(al.curso, 50, stdin);
+    fputs("Curso: ", a);
     fputs(al.curso,a);
 }
 
