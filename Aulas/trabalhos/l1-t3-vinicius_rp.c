e1b03f94d7f48e8a8b091e48bd511abe66041a4e
vinirighi <viniciusluccarighi@gmail.com>
2026-05-27 17:58:19 -0300

    Adicionando funcao leNotas

--- Aulas/trabalhos/l1-t3-vinicius_rp.c
+++ Aulas/trabalhos/l1-t3-vinicius_rp.c
@@ -29,7 +29,7 @@ typedef struct{
 }Nota;
 
 void leEtiqueta(FILE *a, char e[]){
-    if(fscanf(a, "%c%c%c", &e[0], &e[1], &e[2]) != 3)
+    if(fscanf(a, " %c%c%c", &e[0], &e[1], &e[2]) != 3)
         printf("Erro ao ler etiqueta!\n");
 }
 
@@ -37,7 +37,7 @@ void leCor(FILE *a, Cor *c){
     int vermelho;
     int verde;
     int azul;
-    if(fscanf(a, "%d %d %d", &vermelho, &verde, &azul) != 3){
+    if(fscanf(a, "%d%d%d", &vermelho, &verde, &azul) != 3){
         printf("Erro ao ler cor!\n");
         return;
     }
@@ -51,7 +51,7 @@ void leCor(FILE *a, Cor *c){
 }
 
 void leRetangulo(FILE *a, Retangulo *r){
-    if(fscanf(a, "%d %d %d %d", &r->ponto.x, &r->ponto.y, &r->tamanho.largura, &r->tamanho.altura) != 4)
+    if(fscanf(a, "%d%d%d%d", &r->ponto.x, &r->ponto.y, &r->tamanho.largura, &r->tamanho.altura) != 4)
         printf("Erro ao ler Retangulo!\n");
 }
 
@@ -70,8 +70,7 @@ void consumirLinha(FILE *a){
     while((c = fgetc(a)) != '\n' && c != EOF);
 }
 
-Nota leNota(char nome[]){
-    FILE *arq = fopen(nome, "r");
+Nota leNota(FILE *arq){
     Nota n = {0};
 
     if(arq == NULL){
@@ -84,21 +83,55 @@ Nota leNota(char nome[]){
     leTexto(arq, n.texto);
     consumirLinha(arq);
 
-    fclose(arq);
     return n;
 }
 
-void leNotas(char n[]){
-    FILE *arq = fopen(n, "r");
-
+void leNotas(Nota n[], int t, FILE *arq){
+    for(int a = 0; a < t; a++){
+        n[a] = leNota(arq);
+    }
 }
 
 
-
 int main(){
-    char nome[] = "arquivo.txt";
+    FILE *arquivo = fopen("arquivo.txt", "r");
 
-    leNota(nome);
+    if(arquivo == NULL){
+        printf("Erro ao abrir!\n");
+        return 1;
+    }
+
+    Nota notas[3] = {0};
+
+    leNotas(notas, 3, arquivo);
+
+    fclose(arquivo);
+
+    for(int i = 0; i < 3; i++){
+        printf("Nota %d\n", i + 1);
+
+        printf("Etiqueta: %c%c%c\n",
+               notas[i].etiqueta[0],
+               notas[i].etiqueta[1],
+               notas[i].etiqueta[2]);
+
+        printf("Cor: %d %d %d\n",
+               notas[i].cor.r,
+               notas[i].cor.g,
+               notas[i].cor.b);
+
+        printf("Posicao: (%d, %d)\n",
+               notas[i].retangulo.ponto.x,
+               notas[i].retangulo.ponto.y);
+
+        printf("Tamanho: %d x %d\n",
+               notas[i].retangulo.tamanho.largura,
+               notas[i].retangulo.tamanho.altura);
+
+        printf("Texto: %s\n", notas[i].texto);
+
+        printf("\n");
+    }
 
     return 0;
 }
