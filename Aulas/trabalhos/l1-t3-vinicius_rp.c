441cf2afaeefdcc94dc5a5ed6fed82be864e1a9f
vinirighi <viniciusluccarighi@gmail.com>
2026-06-05 22:14:03 -0300

    Adicionando cursor

--- Aulas/trabalhos/l1-t3-vinicius_rp.c
+++ Aulas/trabalhos/l1-t3-vinicius_rp.c
@@ -422,6 +422,19 @@ void desenhaModoPrincipal(Sistema *s){
     printf("Notas encontradas: %d\n", s->validos[0]);
 }
 
+bool cursorDentroDaNota(Cursor *c, Nota *n){
+    return c->x >= n->retangulo.ponto.x && c->x <  n->retangulo.ponto.x + n->retangulo.tamanho.largura &&
+           c->y >= n->retangulo.ponto.y && c->y <  n->retangulo.ponto.y + n->retangulo.tamanho.altura;
+}
+
+int encontraNotaCorrente(Sistema *s){
+    for(int i = s->quantidade - 1; i >= 0; i--){
+        Nota *n = &s->notas[i];
+        if(cursorDentroDaNota(&s->cursor, n)) return i;
+    }
+    return -1;
+}
+
 void modoPrincipal(Sistema *s){
     encontrarValidos(s);
     s->notaCorrente = s->validos[1];
@@ -526,7 +539,6 @@ void modoPrincipal(Sistema *s){
             if(s->validos[0] != 0){
                 s->notaCorrente = s->validos[1];
             }
-            
         }
         if(c == 'E'){
             if(s->validos[0] != 0){
@@ -542,7 +554,24 @@ void modoPrincipal(Sistema *s){
     }
 }
 
+
+
 void modoEditarTexto(Sistema *s){
+    while(s->modo == EDITAR_TEXTO){
+        if(s->notaCorrente != -1){
+
+
+
+
+        }
+
+
+
+    
+    
+    
+    
+    }
     
     s->modo = PRINCIPAL;
 }
